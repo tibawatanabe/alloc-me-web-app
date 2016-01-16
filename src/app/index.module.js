c352a57cda7aece4791a4cc5117db31ed503f55c
Marcelo Montanher <marcelo.montanher14@gmail.com>
2016-01-16 13:06:37 -0200

    Exemplo chart

--- src/app/index.module.js
+++ src/app/index.module.js
@@ -4,5 +4,13 @@
   angular
     .module('allocMe', ['ngAnimate', 'ngTouch', 'ngSanitize', 'ui.router', 'ui.bootstrap', 'allocMe.lib'])
     .config(MainRouter)
+
+    // .state('chart', {
+    //     url: '/chart',
+    //     templateUrl: 'app/chart/chart.html',
+    //     controller: 'ChartController',
+    //     controllerAs: 'chart'
+    //   })
   ;
 })();
+
